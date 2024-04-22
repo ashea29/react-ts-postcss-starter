5ceea8162eb6071da21a7a8545b58686bf3fdb9a
Andrew Shea <andrew.shea783@gmail.com>
2024-04-22 12:41:12 -0600

    added autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,10 @@ export default defineConfig({
   },
   server: {
     open: true
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer()]
+    }
   }
 });
